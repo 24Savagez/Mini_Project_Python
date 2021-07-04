c6065a89d26cea061c057f1a1c5036cc6dcf7864
Chutayu Adhitayawong <73163550+24Savagez@users.noreply.github.com>
2021-07-04 21:06:04 +0700

    file directories and paths

--- day-24/main.py
+++ day-24/main.py
@@ -7,5 +7,10 @@
 #     contents = file.read()
 #     print(contents)
 
-with open("new_file.txt", mode="w") as file:
-    file.write("\nNew text2.")
+# with open("new_file.txt", mode="w") as file:
+#     file.write("\nNew text2.")
+
+# Relative file paths
+with open("../my_file.txt") as file:
+    contents = file.read()
+    print(contents)
