cbd719b6de3ce180fd9d8e728601f0111e9eeb37
Chutayu Adhitayawong <73163550+24Savagez@users.noreply.github.com>
2021-07-08 09:06:28 +0700

    tkinter

--- day-27-start/main.py
+++ day-27-start/main.py
@@ -1,12 +1,31 @@
 import tkinter
+from tkinter import *
 
-window = tkinter.Tk()
+window = Tk()
 window.title("My First GUI Program")
 window.minsize(width=500, height=300)
 
 # Label
-my_label = tkinter.Label(text="I am a Label", font=("Arial", 24, "bold"))
+my_label = Label(text="I am a Label", font=("Arial", 24, "bold"))
 my_label.pack()  # show label
+
+my_label["text"] = "New Text"
+my_label.config(text="New Text")
+
+
+def button_clicker():
+    print("I got clicked")
+    new_text = input.get()
+    my_label.config(text=new_text)
+
+
+# Button
+button = Button(text="Click Me", command=button_clicker)
+button.pack()
+
+# Entry
+input = Entry(width=15)
+input.pack()
 
 # show all time screen
 window.mainloop()
