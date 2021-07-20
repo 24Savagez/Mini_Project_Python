2015c4b08e9191dfa2f42df977c2e4095617493c
Chutayu Adhitayawong <73163550+24Savagez@users.noreply.github.com>
2021-07-20 14:05:12 +0700

    change venv

--- rain_alert/main.py
+++ rain_alert/main.py
@@ -1,16 +1,17 @@
 import requests
 
+OWM_Endpoint = "https://api.openweathermap.org/data/2.5/onecall"
 api_key = "15287b3801bf502176b265c647b3b2a8"
 
 parameters = {
     "lat": 14.039736,
     "lon": 100.613427,
-    "exclude": "hourly",
+    # "exclude": "hourly",
     "appid": api_key
 }
 
-response = requests.get("https://api.openweathermap.org/data/2.5/onecall", params=parameters)
-print(response.raise_for_status())
+response = requests.get(OWM_Endpoint, params=parameters)
+print(response.status_code)
 data = response.json()
 
 print(data)
